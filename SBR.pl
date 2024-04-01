4f3599dd06a65b454d08773b6355f70d9328f6ec
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-04-01 12:10:31 +0200

    Created list

--- SBR.pl
+++ SBR.pl
@@ -1,1 +1,18 @@
-ciencias("Física").
+:- dynamic pair/2.
+
+ciencias("Física") :- add_pair("Física", 1).
+
+
+add_pair(Key, Value) :-
+    not(pair(Key, _)),
+    assert(pair(Key, Value)),
+    !. % Cut to prevent backtracking
+
+add_pair(Key, Value) :-
+    pair(Key, OldValue),
+    NewValue is OldValue + Value,
+    retract(pair(Key, OldValue)), % Remove old pair
+    assert(pair(Key, NewValue)).  % Assert new pair
+
+
+final :- forall(pair(X, _), writef(X)).
